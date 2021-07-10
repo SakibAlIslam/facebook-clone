2c8482a3dfd9b3f7729f311c98719aba57ce3607
Sakib12345 <sakibalislam12345@gmail.com>
2021-07-11 04:46:23 +0600

    added header left side design new

--- components/Header.js
+++ components/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
                 <Image alt="" src="https://links.papareact.com/5me" width={40} height={40} layout="fixed" />
             </div>
             <div className="flex items-center rounded-full ml-2 bg-gray-100 p-2">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4 ml-1 text-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
                 </svg>
                 <input className="flex ml-2 center bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search Facebook" />
